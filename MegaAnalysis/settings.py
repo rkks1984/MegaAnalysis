e6bdb37c91161a3c705bd3844bf9a9315e14992a
Rishi Singh <rkks1984@gmail.com>
2020-09-07 23:01:47 +0200

    First version MegaAnalysis

--- MegaAnalysis/settings.py
+++ MegaAnalysis/settings.py
@@ -77,8 +77,8 @@ WSGI_APPLICATION = 'MegaAnalysis.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django_mongodb_engine',
+        'NAME': ('stock_database'),
     }
 }
 
